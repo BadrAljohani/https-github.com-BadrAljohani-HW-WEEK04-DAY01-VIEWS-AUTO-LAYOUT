579b0894a208b537d2ef7af9cf13ef9c4503e641
Badr Aljohani <Badr.aljohani.med.s@tuwaiq.edu.sa>
2021-10-19 11:17:10 +0300

    Initial Commit

---
+++ Freme_HW1_Week3Tests/Freme_HW1_Week3Tests.swift
@@ -0,0 +1,33 @@
+//
+//  Freme_HW1_Week3Tests.swift
+//  Freme_HW1_Week3Tests
+//
+//  Created by Badr on 19/10/21.
+//
+
+import XCTest
+@testable import Freme_HW1_Week3
+
+class Freme_HW1_Week3Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
